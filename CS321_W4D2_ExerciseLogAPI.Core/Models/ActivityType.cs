7c423a8e77c6091d0f2ed6a7154f0440d14677ae
kemora08 <kevin_mora89@hotmail.com>
2020-06-24 21:10:21 -0500

    Initial Commit

--- CS321_W4D2_ExerciseLogAPI.Core/Models/ActivityType.cs
+++ CS321_W4D2_ExerciseLogAPI.Core/Models/ActivityType.cs
@@ -6,6 +6,17 @@ namespace CS321_W4D2_ExerciseLogAPI.Core.Models
 {
     class ActivityType
     {
+        public enum RecordType
+        {
+            DurationOnly, // only record duration
+            DurationAndDistance // record duration and distance
+        }
 
+        public class ActivityType
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public RecordType RecordType { get; set; }
+        }
     }
 }
